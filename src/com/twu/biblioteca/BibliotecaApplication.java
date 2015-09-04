f6d705d26f5cad45988a6140058c50de5372e9be
deepika <deepikav2793@gmail.com>
2015-09-04 12:29:03 +0530

    Added spec in BibliotecaApplicationTest.java that Biblioteca Application should have a Welcome Message when it starts. Created start() in BibliotecaApplication.java and printed the welcome message

---
+++ src/com/twu/biblioteca/BibliotecaApplication.java
@@ -0,0 +1,11 @@
+/*View displays data to the console and gets data from it*/
+package com.twu.biblioteca;
+
+public class BibliotecaApplication {
+
+    String welcomeMessage = "Hello! Welcome to Bangalore Public Library";
+
+    public void start() {
+        System.out.println(welcomeMessage);
+    }
+}
---
+++ test/com/twu/biblioteca/BibliotecaApplicationTest.java
@@ -0,0 +1,30 @@
+package com.twu.biblioteca;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
+
+public class BibliotecaApplicationTest {
+    private final ByteArrayOutputStream outputContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setStreamsWithInitialValue() {
+        System.setOut(new PrintStream(outputContent));
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(System.out);
+    }
+
+    @Test
+    public void out() {
+        BibliotecaApplication bibliotecaApplication =new BibliotecaApplication();
+        bibliotecaApplication.start();
+        assertEquals("Hello! Welcome to Bangalore Public Library!\n", outputContent.toString());
+    }
+}
