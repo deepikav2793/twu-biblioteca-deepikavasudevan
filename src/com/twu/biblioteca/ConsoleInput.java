d345b8fecf7628eb0fc27b291bcd9eaa135dccd7
deepika <deepikav2793@gmail.com>
2015-09-08 12:11:19 +0530

    Added a spec in MainMenu.java to show the options

---
+++ src/com/twu/biblioteca/ConsoleInput.java
@@ -0,0 +1,12 @@
+/*takes input from console*/
+package com.twu.biblioteca;
+
+import java.util.Scanner;
+
+public class ConsoleInput {
+    Scanner input = new Scanner(System.in);
+
+    public String getInput() {
+        return input.next();
+    }
+}
