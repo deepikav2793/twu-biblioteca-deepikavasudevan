3c044d9a24837bfbc7fee2f0e6aece9b854c5f5b
deepika <deepikav2793@gmail.com>
2015-09-15 18:11:19 +0530

    Added @Before in MovieTest.java

--- test/com/twu/biblioteca/MovieTest.java
+++ test/com/twu/biblioteca/MovieTest.java
@@ -1,5 +1,6 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -7,48 +8,45 @@ import static org.junit.Assert.assertNotEquals;
 
 public class MovieTest {
 
+    private Movie movie;
+
+    @Before
+    public void setUpMovie() {
+        movie = new Movie("Funny Girl", 1968, "William Wyler", "8");
+    }
+
     @Test
     public void shouldFormatItsDetailsInColumnFormat() {
-        Movie movie = new Movie("Funny Girl", 1968, "William Wyler", "8");
-
         assertEquals("Funny Girl\t1968\tWilliam Wyler\t8",movie.toString());
     }
 
     @Test
     public void shouldNotBeEqualToNull() {
-        Movie movie = new Movie("Funny Girl", 1968, "William Wyler", "8");
-
         assertNotEquals(movie, null);
     }
 
     @Test
     public void shouldBeOfTypeMovie() {
-        Movie movie = new Movie("Funny Girl", 1968, "William Wyler", "8");
-
         assertEquals(movie.getClass(), movie.getClass());
     }
 
     @Test
     public void shouldBeEqualToItself() {
-        Movie movie = new Movie("Funny Girl", 1968, "William Wyler", "8");
-
         assertEquals(movie, movie);
     }
 
     @Test
     public void shouldBeEqualToAMovieOfTheSameName() {
-        Movie movie1 = new Movie("Funny Girl", 1968, "William Wyler", "8");
         Movie movie2 = new Movie("Funny Girl", 0, "", "");
 
-        assertEquals(movie1, movie2);
+        assertEquals(movie, movie2);
     }
 
     @Test
     public void shouldNotBeEqualToAMovieOfTheSameName() {
-        Movie movie1 = new Movie("Funny Girl", 1968, "William Wyler", "8");
         Movie movie2 = new Movie("Funny", 1968, "Willian Wyler", "8");
 
-        assertNotEquals(movie1, movie2);
+        assertNotEquals(movie, movie2);
     }
 
     @Test
@@ -60,9 +58,8 @@ public class MovieTest {
 
     @Test
     public void shouldNotHaveTheSameHashCodeIfItIsNotTheSameReference() {
-        Movie movie1 = new Movie("Funny Girl", 1968, "William Wyler", "8");
         Movie movie2 = new Movie("Pretty in Pink", 1967, "John Hughes","10");
 
-        assertNotEquals(movie1.hashCode(), movie2.hashCode());
+        assertNotEquals(movie.hashCode(), movie2.hashCode());
     }
 }
