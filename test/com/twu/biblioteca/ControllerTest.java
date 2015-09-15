0f8e0bf33b81cbe1edde61a1f53859e2be92c4fb
deepika <deepikav2793@gmail.com>
2015-09-15 17:38:30 +0530

    Added @Before in ControllerTest.java. Removed extra space in ConsoleInputTest.java

--- test/com/twu/biblioteca/ControllerTest.java
+++ test/com/twu/biblioteca/ControllerTest.java
@@ -1,5 +1,6 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
 import org.junit.Test;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -9,12 +10,19 @@ import static org.junit.Assert.assertEquals;
 
 public class ControllerTest {
 
-    @Test
-    public void shouldInitialiseWelcomeMessageAndDisplayIt() {
-        ByteArrayOutputStream outputContent = new ByteArrayOutputStream();
+    private ByteArrayOutputStream outputContent;
+    private Controller controller;
+
+    @Before
+    public void setUpOutputContentAndController() {
+        outputContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outputContent));
 
-        Controller controller = new Controller();
+        controller = new Controller();
+    }
+
+    @Test
+    public void shouldInitialiseWelcomeMessageAndDisplayIt() {
         controller.displayWelcomeMessage();
 
         assertEquals("Hello! Welcome to Bangalore Public Library!\n", outputContent.toString());
@@ -22,12 +30,9 @@ public class ControllerTest {
 
     @Test
     public void shouldInitialiseMenuToDisplayItsOptionsAndDispatch() {
-        ByteArrayOutputStream outputContent = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputContent));
         ByteArrayInputStream inputOption = new ByteArrayInputStream("Invalid".getBytes());
         System.setIn(inputOption);
 
-        Controller controller = new Controller();
         controller.displayMenuOptionsAndDispatch();
 
         assertEquals("MAIN MENU\n1. List Books\n2. Checkout Book\n3. Return Book\n4. List Movies\n5. Quit\n\nSelect " +
