ae7a013a838e03fd6cf21d1cc65973978690e857
deepika <deepikav2793@gmail.com>
2015-09-16 11:47:21 +0530

    Added spec in UserTest.java that User should have Unsuccessful Login if user names are not equal and passwords are equal

--- test/com/twu/biblioteca/UserTest.java
+++ test/com/twu/biblioteca/UserTest.java
@@ -8,15 +8,22 @@ public class UserTest {
 
     @Test
     public void shouldHaveSuccessfulLoginWhenLibraryNumberAndPasswordsAreEqual() {
-        User user = new User("xxx-xxxx", "password", "Guest");
+        User user = new User("xxx-xxxx", "password", "GUEST");
 
         assertEquals("Login successful", user.authenticate("xxx-xxxx", "password"));
     }
 
     @Test
     public void shouldHaveUnsuccessfulLoginWhenLibraryNumbersAreNotEqualAndPasswordsAreEqual() {
-        User user = new User("xxx-xxxx", "password", "Guest");
+        User user = new User("xxx-xxxx", "password", "GUEST");
 
         assertEquals("Login unsuccessful", user.authenticate("yyy-yyyy", "password"));
+    }
+
+    @Test
+    public void shouldHaveUnsuccessfulLoginWhenLibraryNumbersAreEqualAndPasswordsAreEqual() {
+        User user = new User("xxx-xxxx", "password", "GUEST");
+
+        assertEquals("Login unsuccessful", user.authenticate("xxx-xxxx", "pass"));
     }
 }
