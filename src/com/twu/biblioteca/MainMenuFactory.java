204fc70d2e739afbc1643f55494ba9262c6db2bd
deepika <deepikav2793@gmail.com>
2015-09-17 00:47:57 +0530

    Added spec in MainMenuFactory that it shoud intialise Menu with options

--- src/com/twu/biblioteca/MainMenuFactory.java
+++ src/com/twu/biblioteca/MainMenuFactory.java
@@ -9,6 +9,15 @@ public class MainMenuFactory {
 
     public MainMenu createMainMenu() {
         ArrayList<String> menuOptions = new ArrayList<>();
+        menuOptions.add("1. Login");
+        menuOptions.add("2. List Books");
+        menuOptions.add("3. Checkout Book");
+        menuOptions.add("4. Return Book");
+        menuOptions.add("5. List Movies");
+        menuOptions.add("6. Checkout Movie");
+        menuOptions.add("7. List Checked Out Books with User's Library Number");
+        menuOptions.add("8. Logout");
+        menuOptions.add("9. Quit");
 
         mainMenu = new MainMenu(menuOptions);
         return mainMenu;
--- test/com/twu/biblioteca/MainMenuFactoryTest.java
+++ test/com/twu/biblioteca/MainMenuFactoryTest.java
@@ -12,4 +12,14 @@ public class MainMenuFactoryTest {
 
         assertEquals(MainMenu.class, mainMenuFactory.createMainMenu().getClass());
     }
+
+    @Test
+    public void shouldInitialiseMainMenuWithOptions() {
+        MainMenuFactory mainMenuFactory = new MainMenuFactory();
+
+        MainMenu mainMenu = mainMenuFactory.createMainMenu();
+
+        assertEquals("MAIN MENU\n1. Login\n2. List Books\n3. Checkout Book\n4. Return Book\n5. List Movies\n6. Checkout Movie" +
+                "\n7. List Checked Out Books with User's Library Number\n8. Logout\n9. Quit\n", mainMenu.mainMenuOptions());
+    }
 }
