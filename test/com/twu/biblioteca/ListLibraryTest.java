b96c9473d0ebabdb615a73d13d9e75e46bee226d
deepika <deepikav2793@gmail.com>
2015-09-17 08:42:19 +0530

    Added a spec that ListLibrary should use Console Output to print its content

--- test/com/twu/biblioteca/ListLibraryTest.java
+++ test/com/twu/biblioteca/ListLibraryTest.java
@@ -3,6 +3,7 @@ package com.twu.biblioteca;
 import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
@@ -53,5 +54,18 @@ public class ListLibraryTest {
         listLibrary.displayList();
 
         verify(movieLibrary, times(1)).list();
+    }
+
+    @Test
+    public void shouldUseConsoleOutputToPrintList(){
+        BookLibrary bookLibrary = mock(BookLibrary.class);
+        when(bookLibrary.list()).thenReturn("NAME OF BOOK\tNAME OF AUTHOR\tYEAR OF PUBLICATION\nTo Kill A Mockingbird\tHarper Lee\t1968\n" +
+                "The Scarlett Letter\tNathaniel Hawthorne\t1850\nGone Girl\tGillian Flynn\t2000\n");
+        ConsoleOutput consoleOutput = mock(ConsoleOutput.class);
+        ListLibrary listLibrary = new ListLibrary(bookLibrary, consoleOutput);
+        listLibrary.displayList();
+
+        verify(consoleOutput, times(1)).display("NAME OF BOOK\tNAME OF AUTHOR\tYEAR OF PUBLICATION\nTo Kill A Mockingbird\tHarper Lee\t1968\n" +
+                        "The Scarlett Letter\tNathaniel Hawthorne\t1850\nGone Girl\tGillian Flynn\t2000\n");
     }
 }
