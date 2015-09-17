5a0bb370b869dcd0b9cdfa256f3b4f87f0006e9e
deepika <deepikav2793@gmail.com>
2015-09-17 08:31:42 +0530

    Added a spec that List Library should List Movie Library's books is Movie Library is being passed

--- test/com/twu/biblioteca/ListLibraryTest.java
+++ test/com/twu/biblioteca/ListLibraryTest.java
@@ -23,4 +23,18 @@ public class ListLibraryTest {
         assertEquals("NAME OF BOOK\tNAME OF AUTHOR\tYEAR OF PUBLICATION\nTo Kill A Mockingbird\tHarper Lee\t1968\n" +
                 "The Scarlett Letter\tNathaniel Hawthorne\t1850\nGone Girl\tGillian Flynn\t2000\n\n", outputContent.toString());
     }
+
+    @Test
+    public void shouldPrintAListOfMoviesIfLibraryIsMovieLibrary() {
+        ByteArrayOutputStream outputContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputContent));
+        MovieLibrary movieLibrary = mock(MovieLibrary.class);
+        when(movieLibrary.list()).thenReturn("NAME OF MOVIE\tYEAR\tDIRECTOR\tMOVIE RATING\n" +
+                "Funny Girl\t1968\tWilliam Wyler\t8\nPretty in Pink\t1986\tJohn Hughes\t10\n");
+        ConsoleOutput consoleOutput = new ConsoleOutput();
+        ListLibrary listLibrary = new ListLibrary(movieLibrary, consoleOutput);
+        listLibrary.displayList();
+        assertEquals("NAME OF MOVIE\tYEAR\tDIRECTOR\tMOVIE RATING\n" +
+                "Funny Girl\t1968\tWilliam Wyler\t8\nPretty in Pink\t1986\tJohn Hughes\t10\n\n", outputContent.toString());
+    }
 }
