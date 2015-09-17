b812a96edaebecb1ef6882400acd1834e72b5137
deepika <deepikav2793@gmail.com>
2015-09-17 21:50:23 +0530

    Added a spec that Not Authorised Menu Option should use Console Output to print its output

--- test/com/twu/biblioteca/NotAuthorisedMenuOptionTest.java
+++ test/com/twu/biblioteca/NotAuthorisedMenuOptionTest.java
@@ -6,6 +6,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class NotAuthorisedMenuOptionTest {
 
@@ -20,5 +21,15 @@ public class NotAuthorisedMenuOptionTest {
         notAuthorisedMenuOption.executeOptionOperation();
 
         assertEquals("NOT AUTHORISED to access this option. Please log in.\n", outputContent.toString());
+    }
+
+    @Test
+    public void shouldUseConsoleOutputToPrintTheMessage() {
+        User currentUser = new User("Guest User","No Password",ROLE.GUEST_USER);
+        ConsoleOutput consoleOutput = mock(ConsoleOutput.class);
+        NotAuthorisedMenuOption notAuthorisedMenuOption = new NotAuthorisedMenuOption(consoleOutput, currentUser);
+        notAuthorisedMenuOption.executeOptionOperation();
+
+        verify(consoleOutput, times(1)).display("NOT AUTHORISED to access this option. Please log in.");
     }
 }
