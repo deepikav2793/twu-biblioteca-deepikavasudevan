34ed6d9e6b5c2a1cccb7ce29d602765833fd3659
deepika <deepikav2793@gmail.com>
2015-09-18 18:36:48 +0530

    Refactored Controller dispatch into private methods

--- src/com/twu/biblioteca/Controller.java
+++ src/com/twu/biblioteca/Controller.java
@@ -45,46 +45,70 @@ public class Controller {
 
     public MainMenuOption parseMenuOption(String menuOption) {
         if (menuOption.equals("1")) {
-            if (currentUser.getUserRole().equals(ROLE.GUEST_USER))
-                return new LoginMenuOption(consoleInput, consoleOutput, listOfUsers, currentUser);
-            else
-                return new NotAuthorisedMenuOption(consoleOutput, currentUser);
+            return dispatchValidMenuOptionForOptionOne();
         } else if (menuOption.equals("2")) {
             return new ListLibraryMenuOption(bookLibrary, consoleOutput, currentUser);
         } else if (menuOption.equals("3")) {
-            if (currentUser.getUserRole().equals(ROLE.GUEST_USER))
-                return new NotAuthorisedMenuOption(consoleOutput, currentUser);
-            else
-                return new CheckOutMenuOption(bookLibrary, consoleInput, consoleOutput, currentUser);
+            return dispatchValidMenuOptionForOptionThree();
         } else if (menuOption.equals("4")) {
-            if (currentUser.getUserRole().equals(ROLE.GUEST_USER))
-                return new NotAuthorisedMenuOption(consoleOutput, currentUser);
-            else
-                return new ReturnBookMenuOption(bookLibrary, consoleInput, consoleOutput, currentUser);
+            return dispatchValidMenuOptionForOptionFour();
         } else if (menuOption.equals("5")) {
             return new ListLibraryMenuOption(movieLibrary, consoleOutput, currentUser);
         } else if (menuOption.equals("6")) {
             return new CheckOutMenuOption(movieLibrary, consoleInput, consoleOutput, currentUser);
         } else if (menuOption.equals("7")) {
-            if (currentUser.getUserRole().equals(ROLE.LIBRARIAN))
-                return new CheckedOutListWithUser(bookLibrary, consoleOutput, currentUser);
-            else
-                return new NotAuthorisedMenuOption(consoleOutput, currentUser);
+            return dispatchValidMenuOptionForOptionSeven();
         } else if (menuOption.equals("8")) {
-            if (currentUser.getUserRole().equals(ROLE.GUEST_USER))
-                return new NotAuthorisedMenuOption(consoleOutput, currentUser);
-            else
-                return new UserInformationOption(consoleOutput, currentUser);
+            return dispatchValidMenuOptionForOptionEight();
         } else if (menuOption.equals("9")) {
-            if (currentUser.getUserRole().equals(ROLE.GUEST_USER))
-                return new NotAuthorisedMenuOption(consoleOutput, currentUser);
-            else
-                return new LogoutMenuOption(consoleOutput, currentUser);
+            return dispatchValidMenuOptionForMenuOptionNine();
         } else if (menuOption.equals("10")) {
             return new QuitMenuOption(currentUser);
         } else {
             return new InvalidMenuOption(consoleOutput, currentUser);
         }
+    }
+
+    private MainMenuOption dispatchValidMenuOptionForMenuOptionNine() {
+        if (currentUser.getUserRole().equals(ROLE.GUEST_USER))
+            return new NotAuthorisedMenuOption(consoleOutput, currentUser);
+        else
+            return new LogoutMenuOption(consoleOutput, currentUser);
+    }
+
+    private MainMenuOption dispatchValidMenuOptionForOptionEight() {
+        if (currentUser.getUserRole().equals(ROLE.GUEST_USER))
+            return new NotAuthorisedMenuOption(consoleOutput, currentUser);
+        else
+            return new UserInformationOption(consoleOutput, currentUser);
+    }
+
+    private MainMenuOption dispatchValidMenuOptionForOptionSeven() {
+        if (currentUser.getUserRole().equals(ROLE.LIBRARIAN))
+            return new CheckedOutListWithUser(bookLibrary, consoleOutput, currentUser);
+        else
+            return new NotAuthorisedMenuOption(consoleOutput, currentUser);
+    }
+
+    private MainMenuOption dispatchValidMenuOptionForOptionFour() {
+        if (currentUser.getUserRole().equals(ROLE.GUEST_USER))
+            return new NotAuthorisedMenuOption(consoleOutput, currentUser);
+        else
+            return new ReturnBookMenuOption(bookLibrary, consoleInput, consoleOutput, currentUser);
+    }
+
+    private MainMenuOption dispatchValidMenuOptionForOptionThree() {
+        if (currentUser.getUserRole().equals(ROLE.GUEST_USER))
+            return new NotAuthorisedMenuOption(consoleOutput, currentUser);
+        else
+            return new CheckOutMenuOption(bookLibrary, consoleInput, consoleOutput, currentUser);
+    }
+
+    private MainMenuOption dispatchValidMenuOptionForOptionOne() {
+        if (currentUser.getUserRole().equals(ROLE.GUEST_USER))
+            return new LoginMenuOption(consoleInput, consoleOutput, listOfUsers, currentUser);
+        else
+            return new NotAuthorisedMenuOption(consoleOutput, currentUser);
     }
 
     public void initialiseBibliotecaApplication() {
