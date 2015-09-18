f31a12a947367001bd83e0df73a952c5cd424552
deepika <deepikav2793@gmail.com>
2015-09-18 07:51:30 +0530

    Added a spec that CheckedOUtListWithUser should use Console Output for displaying purposes

--- test/com/twu/biblioteca/CheckedOutListWithUserTest.java
+++ test/com/twu/biblioteca/CheckedOutListWithUserTest.java
@@ -6,8 +6,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class CheckedOutListWithUserTest {
 
@@ -27,5 +26,21 @@ public class CheckedOutListWithUserTest {
         checkedOutListWithUser.executeOptionOperation();
 
         assertEquals(formattedListOfBooks + "\n", outputContent.toString());
+    }
+
+    @Test
+    public void shouldUseConsoleOutputToDisplayListOfCheckedOutBooks() {
+        ConsoleOutput consoleOutput = mock(ConsoleOutput.class);
+        BookLibrary bookLibrary = mock(BookLibrary.class);
+        String formattedListOfBooks = String.format("%-30s%-30s%-15s\n", "NAME OF BOOK", "NAME OF AUTHOR", "YEAR OF PUBLICATION")
+                + String.format("%-30s%-30s%-15s\n", "To Kill A Mockingbird", "Harper Lee", 1968)
+                + String.format("%-30s%-30s%-15s\n", "Gone Girl", "Gillian Flynn", 2000)
+                + String.format("%-30s%-30s%-15s\n", "The Scarlett Letter", "Nathaniel Hawthorne", 1850);
+        when(bookLibrary.checkedOutBookListWithUser()).thenReturn(formattedListOfBooks);
+        User currentUser = new User("Guest User", "No Password", ROLE.GUEST_USER);
+        CheckedOutListWithUser checkedOutListWithUser = new CheckedOutListWithUser(bookLibrary, consoleOutput, currentUser);
+        checkedOutListWithUser.executeOptionOperation();
+
+        verify(consoleOutput, times(1)).display(formattedListOfBooks);
     }
 }
